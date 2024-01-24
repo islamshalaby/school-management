bfe38d859fd676af5fcf77fb7d1777696977f81c
islamshalaby <islamshalby510@gmail.com>
2024-01-24 10:55:39 +0200

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { LessonModule } from './lesson/lesson.module';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://localhost/school',
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+    LessonModule,
+  ],
+})
+export class AppModule {}
---
+++ src/lesson/lesson.entity.ts
@@ -0,0 +1,24 @@
+import {
+  Column,
+  Entity,
+  ObjectIdColumn,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class Lesson {
+  @ObjectIdColumn()
+  _id: string;
+
+  @PrimaryGeneratedColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  startDate: string;
+
+  @Column()
+  endDate: string;
+}
